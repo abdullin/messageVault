490a039105944cb6c4996b8d6f629ef74ba899fe
Rinat Abdullin <rinat.abdullin@gmail.com>
2015-05-07 12:36:44 +0500

    LZ4: switched to unsafe version (faster)

--- src/MessageVault.Core/StorageFormat.cs
+++ src/MessageVault.Core/StorageFormat.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using LZ4s;
 
 namespace MessageVault {
 
--- src/MessageVault/Api/PagedClient.cs
+++ src/MessageVault/Api/PagedClient.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Threading;
-using LZ4s;
+using LZ4n;
 
 namespace MessageVault.Api {
 

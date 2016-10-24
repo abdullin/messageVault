f86f74e93c545bb49717e2d60393b5029bf3db30
Rinat Abdullin <rinat.abdullin@gmail.com>
2016-10-24 19:52:49 +0500

    client: direct reader

---
+++ src/MessageVault/Api/DirectFetcher.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MessageVault.Cloud;
+using MessageVault.Files;
+using MessageVault.MemoryPool;
+
+namespace MessageVault.Api {
+
+	public sealed class DirectFetcher
+	{
+		readonly CloudPageReader _remote;
+		readonly CloudCheckpointReader _remotePos;
+
+
+		readonly IMemoryStreamManager _streamManager;
+		public readonly string StreamName;
+
+
+		
+		public DirectFetcher(string sas, string stream, IMemoryStreamManager streamManager = null)
+		{
+			StreamName = stream;
+			_streamManager = streamManager ?? new MemoryStreamFactoryManager();
+			var raw = CloudSetup.GetReaderRaw(sas);
+
+			_remote = raw.Item2;
+			_remotePos = raw.Item1;
+
+		}
+
+		static bool TryRead(BinaryReader reader, out MessageWithId msg)
+		{
+			try
+			{
+				msg = StorageFormat.Read(reader);
+				return true;
+			}
+			catch (EndOfStreamException)
+			{
+				msg = null;
+				return false;
+			}
+		}
+
+		public int AmountToLoadMax = 10 * 1024 * 1024;
+
+
+		public async Task<DirectReadBulkResult> ReadAll(CancellationToken token, long startingFrom, int maxCount)
+		{
+			var result = new DirectReadBulkResult();
+			var stats = await ReadAll(token, startingFrom, maxCount, (id, position, maxPosition) => {
+				if (result.Messages == null)
+				{
+					result.Messages = new List<MessageHandlerClosure>();
+				}
+				result.Messages.Add(new MessageHandlerClosure
+				{
+					CurrentCachePosition = position,
+					MaxCachePosition = maxPosition,
+					Message = id
+				});
+			}).ConfigureAwait(false);
+
+			result.CurrentPosition = stats.CurrentPosition;
+			result.MaxPosition = stats.MaxPosition;
+			result.ReadRecords = stats.ReadRecords;
+			result.StartingPosition = stats.StartingPosition;
+			return result;
+		}
+
+		public async Task<DirectReadResult> ReadAll(CancellationToken token, long startingFrom, int maxCount, MessageHandler handler)
+		{
+			//var convertedLocalPos = pos[0];
+			//var currentRemotePosition = pos[1];
+
+
+			var maxPos = await _remotePos.ReadAsync(token)
+				.ConfigureAwait(false);
+			var result = new DirectReadResult
+			{
+				StartingPosition = startingFrom,
+				CurrentPosition = startingFrom,
+				MaxPosition = maxPos,
+			};
+
+			if (maxPos <= startingFrom)
+			{
+				// we don't have anything to write
+				return result;
+			}
+
+			var availableAmount = maxPos - startingFrom;
+			if (availableAmount <= 0)
+			{
+				// we don't have anything to write
+				return result;
+			}
+
+			var amountToLoad = Math.Min(availableAmount, AmountToLoadMax);
+
+			using (var mem = _streamManager.GetStream("fetcher"))
+			{
+				await _remote.DownloadRangeToStreamAsync(mem, startingFrom, (int)amountToLoad)
+					.ConfigureAwait(false);
+				// cool, we've got some data back
+
+				mem.Seek(0, SeekOrigin.Begin);
+
+				var usedBytes = 0L;
+
+				var pages = new List<MessageWithId>();
+				using (var bin = new BinaryReader(mem))
+				{
+					MessageWithId msg;
+					while (TryRead(bin, out msg))
+					{
+						var hasMorePagesToRead = HasMore(msg);
+
+						if (false == hasMorePagesToRead && pages.Count == 0)
+						{
+							// fast path, we can save message directly without merging
+							handler(msg,msg.Id.GetOffset(),maxPos);
+
+							//WriteMessage(msg);
+							usedBytes = mem.Position;
+							result.ReadRecords += 1;
+							
+							continue;
+						}
+
+						pages.Add(msg);
+						if (hasMorePagesToRead)
+						{
+							continue;
+						}
+
+						var total = pages.Sum(m => m.Value.Length);
+						using (var sub = _streamManager.GetStream("chase-1", total))
+						{
+							foreach (var page in pages)
+							{
+								sub.Write(page.Value, 0, page.Value.Length);
+							}
+							sub.Seek(0, SeekOrigin.Begin);
+							var last = pages.Last();
+							var final = new MessageWithId(last.Id, last.Attributes, last.Key, sub.ToArray(), 0);
+							handler(final, last.Id.GetOffset(), maxPos);
+							usedBytes = mem.Position;
+							result.ReadRecords += 1;
+							
+						}
+						pages.Clear();
+
+					}
+
+				}
+				result.CurrentPosition = result.StartingPosition + usedBytes;
+				return result;
+			}
+		}
+
+
+
+
+
+		static bool HasMore(MessageWithId msg)
+		{
+			var hasMore = ((MessageFlags)msg.Attributes & MessageFlags.ToBeContinued) ==
+			              MessageFlags.ToBeContinued;
+			return hasMore;
+		}
+	}
+
+}
